d251a24489b5340e891c1475683e88e25bc41ef3
YasserAlwlawdly <yasser.m@msn.com>
2021-10-16 15:50:07 +0200

    new dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -2,209 +2,65 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 {{--<meta name="robots" content="all,follow">--}}
 
 <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }} Dashboard</title>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@300;400;700&amp;display=swap">
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Martel+Sans:wght@300;400;800&amp;display=swap">
-    <!-- FontAwesome CSS - loading as last, so it doesn't block rendering-->
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
-          integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link href="{{ asset('backend/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('frontend/vendor/lightbox2/css/lightbox.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/vendor/nouislider/nouislider.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/vendor/bootstrap-select/css/bootstrap-select.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/vendor/owl.carousel2/assets/owl.carousel.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/vendor/owl.carousel2/assets/owl.theme.default.css') }}">
-    <link rel="stylesheet" href="{{ asset('frontend/css/style.default.css') }}" id="theme-stylesheet">
-    <link rel="stylesheet" href="{{ asset('frontend/css/custom.css') }}">
-    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+    <!-- Custom fonts for this template-->
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('backend/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    @yield('style')
 </head>
-<body>
-<div id="app" class="page-holder {{ request()->routeIs('frontend.detail') ? ' bg-light' : null }}">
+<body id="page-top">
+<div class="app">
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+    @include('partial.backend.sidebar')
 
-    <!-- navbar-->
-    <header class="header bg-white">
-        <div class="container px-0 px-lg-3">
-            <nav class="navbar navbar-expand-lg navbar-light py-3 px-lg-0"><a class="navbar-brand"
-                                                                              href="{{ route('frontend.index') }}"><span
-                        class="font-weight-bold text-uppercase text-dark">Boutique</span></a>
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <!-- Link--><a class="nav-link active" href="{{ route('frontend.index') }}">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <!-- Link--><a class="nav-link" href="{{ route('frontend.shop') }}">Shop</a>
-                        </li>
-                        <li class="nav-item">
-                            <!-- Link--><a class="nav-link" href="{{ route('frontend.detail') }}">Product detail</a>
-                        </li>
-                        <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" id="pagesDropdown" href="#"
-                                                         data-toggle="dropdown" aria-haspopup="true"
-                                                         aria-expanded="false">Pages</a>
-                            <div class="dropdown-menu mt-3" aria-labelledby="pagesDropdown"><a
-                                    class="dropdown-item border-0 transition-link" href="{{ route('frontend.index') }}">Homepage</a><a
-                                    class="dropdown-item border-0 transition-link" href="{{ route('frontend.shop') }}">Category</a><a
-                                    class="dropdown-item border-0 transition-link"
-                                    href="{{ route('frontend.detail') }}">Product detail</a><a
-                                    class="dropdown-item border-0 transition-link" href="{{ route('frontend.cart') }}">Shopping
-                                    cart</a><a class="dropdown-item border-0 transition-link"
-                                               href="{{ route('frontend.checkout') }}">Checkout</a></div>
-                        </li>
-                    </ul>
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="{{ route('frontend.cart') }}"> <i
-                                    class="fas fa-dolly-flatbed mr-1 text-gray"></i>Cart<small
-                                    class="text-gray">(2)</small></a></li>
-                        <li class="nav-item"><a class="nav-link" href="#"> <i class="far fa-heart mr-1"></i><small
-                                    class="text-gray"> (0)</small></a></li>
-                        <li class="nav-item"><a class="nav-link" href="#"> <i
-                                    class="fas fa-user-alt mr-1 text-gray"></i>Login</a></li>
-                    </ul>
+    <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="content">
+            @include('partial.backend.navbar')
+            <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    @yield('content')
                 </div>
-            </nav>
-        </div>
-    </header>
-    <!--  Modal -->
 
-    <main class="container">
-        @yield('content')
-    </main>
-
-    <footer class="bg-dark text-white">
-        <div class="container py-4">
-            <div class="row py-5">
-                <div class="col-md-4 mb-3 mb-md-0">
-                    <h6 class="text-uppercase mb-3">Customer services</h6>
-                    <ul class="list-unstyled mb-0">
-                        <li><a class="footer-link" href="#">Help &amp; Contact Us</a></li>
-                        <li><a class="footer-link" href="#">Returns &amp; Refunds</a></li>
-                        <li><a class="footer-link" href="#">Online Stores</a></li>
-                        <li><a class="footer-link" href="#">Terms &amp; Conditions</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-4 mb-3 mb-md-0">
-                    <h6 class="text-uppercase mb-3">Company</h6>
-                    <ul class="list-unstyled mb-0">
-                        <li><a class="footer-link" href="#">What We Do</a></li>
-                        <li><a class="footer-link" href="#">Available Services</a></li>
-                        <li><a class="footer-link" href="#">Latest Posts</a></li>
-                        <li><a class="footer-link" href="#">FAQs</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-4">
-                    <h6 class="text-uppercase mb-3">Social media</h6>
-                    <ul class="list-unstyled mb-0">
-                        <li><a class="footer-link" href="#">Twitter</a></li>
-                        <li><a class="footer-link" href="#">Instagram</a></li>
-                        <li><a class="footer-link" href="#">Tumblr</a></li>
-                        <li><a class="footer-link" href="#">Pinterest</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="border-top pt-4" style="border-color: #1d1d1d !important">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <p class="small text-muted mb-0">&copy; 2020 All rights reserved.</p>
-                    </div>
-                    <div class="col-lg-6 text-lg-right">
-                        <p class="small text-muted mb-0">Template designed by <a class="text-white reset-anchor"
-                                                                                 href="https://bootstraptemple.com/p/bootstrap-ecommerce">Bootstrap
-                                Temple</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-
-</div>
-
-
-<div class="modal fade" id="productView" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-body p-0">
-                <div class="row align-items-stretch">
-                    <div class="col-lg-6 p-lg-0"><a class="product-view d-block h-100 bg-cover bg-center"
-                                                    style="background: url({{ asset('frontend/img/product-5.jpg') }})"
-                                                    href="{{ asset('frontend/img/product-5.jpg') }}"
-                                                    data-lightbox="productview" title="Red digital smartwatch"></a><a
-                            class="d-none" href="{{ asset('frontend/img/product-5-alt-1.jpg') }}"
-                            title="Red digital smartwatch" data-lightbox="productview"></a><a class="d-none"
-                                                                                              href="{{ asset('frontend/img/product-5-alt-2.jpg') }}"
-                                                                                              title="Red digital smartwatch"
-                                                                                              data-lightbox="productview"></a>
-                    </div>
-                    <div class="col-lg-6">
-                        <button class="close p-4" type="button" data-dismiss="modal" aria-label="Close"><span
-                                aria-hidden="true">×</span></button>
-                        <div class="p-5 my-md-4">
-                            <ul class="list-inline mb-2">
-                                <li class="list-inline-item m-0"><i class="fas fa-star small text-warning"></i></li>
-                                <li class="list-inline-item m-0"><i class="fas fa-star small text-warning"></i></li>
-                                <li class="list-inline-item m-0"><i class="fas fa-star small text-warning"></i></li>
-                                <li class="list-inline-item m-0"><i class="fas fa-star small text-warning"></i></li>
-                                <li class="list-inline-item m-0"><i class="fas fa-star small text-warning"></i></li>
-                            </ul>
-                            <h2 class="h4">Red digital smartwatch</h2>
-                            <p class="text-muted">$250</p>
-                            <p class="text-small mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut
-                                ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis
-                                parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.</p>
-                            <div class="row align-items-stretch mb-4">
-                                <div class="col-sm-7 pr-sm-0">
-                                    <div class="border d-flex align-items-center justify-content-between py-1 px-3">
-                                        <span class="small text-uppercase text-gray mr-4 no-select">Quantity</span>
-                                        <div class="quantity">
-                                            <button class="dec-btn p-0"><i class="fas fa-caret-left"></i></button>
-                                            <input class="form-control border-0 shadow-0 p-0" type="text" value="1">
-                                            <button class="inc-btn p-0"><i class="fas fa-caret-right"></i></button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-5 pl-sm-0"><a
-                                        class="btn btn-dark btn-sm btn-block h-100 d-flex align-items-center justify-content-center px-0"
-                                        href="{{ route('frontend.cart') }}">Add to cart</a></div>
-                            </div>
-                            <a class="btn btn-link text-dark p-0" href="#"><i class="far fa-heart mr-2"></i>Add to wish
-                                list</a>
-                        </div>
-                    </div>
-                </div>
+                @include('partial.backend.footer')
             </div>
         </div>
     </div>
 </div>
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+</a>
 
+@include('partial.backend.modal')
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
-<script src="{{ asset('frontend/vendor/lightbox2/js/lightbox.min.js') }}"></script>
-<script src="{{ asset('frontend/vendor/nouislider/nouislider.min.js') }}"></script>
-<script src="{{ asset('frontend/vendor/bootstrap-select/js/bootstrap-select.min.js') }}"></script>
-<script src="{{ asset('frontend/vendor/owl.carousel2/owl.carousel.min.js') }}"></script>
-<script src="{{ asset('frontend/vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js') }}"></script>
-<script src="{{ asset('frontend/js/front.js') }}"></script>
+<!-- Core plugin JavaScript-->
+<script src="{{ asset('backend/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
+<!-- Custom scripts for all pages-->
+<script src="{{ asset('backend/js/sb-admin-2.min.js') }}"></script>
+@yield('script')
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/cart' , [FrontendController::class , 'cart'])->name('frontend.cart'
 Route::get('/checkout' , [FrontendController::class , 'checkout'])->name('frontend.checkout');
 Route::get('/detail' , [FrontendController::class , 'detail'])->name('frontend.detail');
 Route::get('/shop' , [FrontendController::class , 'shop'])->name('frontend.shop');
+
 Route::get('/admin/login' , [BackendController::class , 'login'])->name('backend.login');
 Route::get('/admin/forgot-password' , [BackendController::class , 'forgot_password'])->name('backend.forgot_password');
 Route::get('/admin/index' , [BackendController::class , 'index'])->name('backend.index');
